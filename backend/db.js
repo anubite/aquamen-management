d2e87c10bfb1baa5e4b00deb2608922d852355ea
jirka <jirka@debian.jirka>
2026-02-13 05:25:54 -0500

    feat: support SQLITE_DB_PATH env var for persistent storage

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'aquamen.sqlite');
+const dbPath = process.env.SQLITE_DB_PATH || path.resolve(__dirname, 'aquamen.sqlite');
 const db = new sqlite3.Database(dbPath);
 
 const initDb = () => {
